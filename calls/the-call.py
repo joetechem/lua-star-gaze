712a04676ddaa00a0be2f86a1fa6aff6a0c5b296
joe <weinzjseiler@gmail.com>
2019-01-31 20:27:37 -0500

    refined look at specific item

--- calls/the-call.py
+++ calls/the-call.py
@@ -19,3 +19,10 @@ print("Total repos: ", response_dict['total_count'])
 # Check out certain items from the repositories
 repo_dictionaries = response_dict['items']
 print("Repos returned: ", len(repo_dictionaries))
+
+# Refined look
+# Check out the first repository
+repo_dictionary = repo_dictionaries[0]
+print("\nKeys: ", len(repo_dictionary))
+for key in sorted(repo_dictionary.keys()):
+    print(key)
