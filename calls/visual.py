d030f3b790fe332067383f0dfbdcced8d1233af8
joetechem <weinzjseiler@gmail.com>
2019-02-01 13:04:00 -0500

    visualize the results

--- calls/visual.py
+++ calls/visual.py
@@ -1,31 +1,34 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # stateless API call and capture response
 url = 'https://api.github.com/search/repositories?q=language:lua&sort=stars'
-# call get(), pass the url to this function
 r = requests.get(url)
-# Test whether or not the call is successful
 print("Status Code: ", r.status_code)
-
-# Store the response as the value of a variable
 response_dict = r.json()
-
-# Output the results
-# uncomment below for initial display test
-##print(response_dict.keys())
 
 print("Total repos: ", response_dict['total_count'])
 
-# Check out certain items from the repositories
 repo_dictionaries = response_dict['items']
-print("Repos returned: ", len(repo_dictionaries))
+#print("Repos returned: ", len(repo_dictionaries))
 
-# Refined look
-# Check out the first repository
+names, stars = [], []
+for repo_dictionary in repo_dictionaries:
+    names.append(repo_dictionary['name'])
+    stars.append(repo_dictionary['stargazers_count'])
+
+# Build the visualization
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend = False)
+chart.title = 'Most-Starred Lua Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('lua_repos.svg')
+
 repo_dictionary = repo_dictionaries[0]
-##print("\nKeys: ", len(repo_dictionary))
-##for key in sorted(repo_dictionary.keys()):
-##    print(key)
+
 print("\nSelected info about first repo:")
 print('Name: ', repo_dictionary['name'])
 print('Owner: ', repo_dictionary['owner']['login'])
