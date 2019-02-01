a49019d9afa9293f24f6fb979455cc5ae2453cb5
joe <weinzjseiler@gmail.com>
2019-01-31 20:19:43 -0500

    store reponse and return it

--- calls/the-call.py
+++ calls/the-call.py
@@ -2,6 +2,13 @@ import requests
 
 # stateless API call and capture response
 url = 'https://api.github.com/search/repositories?q=language:lua&sort=stars'
+# call get(), pass the url to this function
 r = requests.get(url)
 # Test whether or not the call is successful
 print("Status Code: ", r.status_code)
+
+# Store the response as the value of a variable
+response_dict = r.json()
+
+# Output the results
+print(response_dict.keys())
